9f891e1bec624cdd764997cc763dab901d56208d
younusFoysal <younus.faysal@gmail.com>
2024-01-27 16:16:10 +0600

    solved 5

--- 5 ternary-number.js
+++ 5 ternary-number.js
@@ -3,10 +3,32 @@
 you have two numbers in two variables, called: num1, num2
 
 now declare a variable called result. 
-if num1 is bigger than num2 then result will be double of num1. if not, then the value of the variable result will be the sum of num1 and num2.
+if num1 is bigger than num2 then result will be double of num1.
+if not, then the value of the variable result will be the sum of num1 and num2.
 
 write a simple if-else. 
 
 also, write it using ternary operator.
 
  */
+
+let num1 = 50;
+let num2 = 62;
+
+let result = 0;
+
+
+if(num1 > num2){
+    console.log(num1*2);
+}else{
+    result = num1 + num2;
+    console.log(result);
+}
+
+(num1 > num2)?console.log(num1*2):console.log(result = num1 + num2);
+
+
+
+
+
+
