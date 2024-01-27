39b38b02afc96b1ab25aa4b13b9a23c19f408363
younusFoysal <younus.faysal@gmail.com>
2024-01-27 16:04:45 +0600

    solved 4

--- 4 nested-friend.js
+++ 4 nested-friend.js
@@ -10,3 +10,19 @@ if you get less than 80 go to home and sleep and act sad
 Note: 
 use nested if-else-if-else
 */
+
+let friendscore = 35;
+
+if (friendscore > 30){
+    if (friendscore > 80){
+        console.log("go for a lunch.");
+    }else if ((friendscore < 80) && (friendscore >= 60)){
+        console.log(" good luck next time. ");
+    }else if ((friendscore < 60) && (friendscore >= 40)){
+        console.log("message unseen.")
+    }else if (friendscore < 40){
+        console.log("block your friend")
+    }
+}else {
+    console.log("go to home and sleep and act sad");
+}
